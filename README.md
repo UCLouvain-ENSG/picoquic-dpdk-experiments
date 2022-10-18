b58a37cc96d1a5f855ab86734ff499a5ddbccecb
kikito <nikita.tyunyayev@student.uclouvain.be>
2022-10-18 13:56:23 +0000

    Working on README and scripts to ease the testing

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-This repository contains tools to compare multiple QUIC implementations
+# README
+This project is an ongoing work to monitor the performance of QUIC implementations. Currently the repository compares the following QUIC implementations : quicly, quiche, msquic, picoquic, picoquic-dpdk. To get a performance reference the repository also contains picotls. This project is an ongoing work to monitor the evolution of QUIC stacks, in the futur other stacks will be added. The
+
+
+# USAGE
+
+This project relies on [NPF](https://github.com/tbarbette/npf) to build the different repositories, launch the measurements and graph the results. The build process was not tested on many environments and may need modifications to fit your setup. The scripts [run_npf.py](run_npf.py) can be used to launch a comparison between selected stacks.
+
+Example :
+
+```
+#launches a comparson between picoquic and msquic
+python3 npf_run.py --stacks picoquic msquic --client client --server server
+```
+
+ 
+# CLUSTER
+
+the cluster repository contains configs to connect to node through SSH. By passing --client=client and --server=server to run_npf.py those files will be used. It is also possible to specified a machine to use directly
+
+Example :
+``` 
+--client=user01@server01.network.edu
+```
+
+# PICOQUIC_DPDK
+
+picoquc_dpdk relies on the [DPDK](https://www.dpdk.org/) library. When running the scripts, it is expected that the library is installed and added to *LD_LIBRARY_PATH*.
+
+
+
+
---
+++ run_npf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import sys
+import argparse
+from subprocess import Popen, PIPE
+
+def npf_runner(stacks,client,server):
+    supported_stack = ['picoquic','picoquic-dpdk','msquic','quiche','quicly','picotls']
+    cmd = "python3 npf/npf-compare.py {}--test quic_tester_compare.npf --cluster client={} server={} --force-build"
+    used_stacks = ''
+
+    for stack in stacks:
+        if stack not in supported_stack:
+            print("Stack {} is not supported".format(stack))
+            print("The supported stacks are : {}".format(supported_stack))
+            return -1
+        elif stack == 'picoquic' or stack == 'picoquic-dpdk':
+            print("hello")
+            #building picotls requiered by picoquic and picoquic-dpdk
+            picotls_cmd = "python3 npf/npf-compare.py picotls --test quic_tester_compare.npf --cluster client={} server={} --no-tests --force-build".format(client,server)
+            p = subprocess.Popen((cmd + '--no-tests'.format('picotls').split(), stdout=subprocess.PIPE)
+            p.wait()
+        else: 
+            used_stacks+='{}+{} '.format(stack,stack)
+    with Popen(cmd.format(used_stacks,client,server).split(), stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            print(line, end='') # process line here
+    
+        
+
+# Required positional argument
+
+parser = argparse.ArgumentParser()
+
+# Stacks used can be specified with --stacks
+parser.add_argument('--stacks','--list', nargs='+', help='<Required> Set flag', required=True)
+parser.add_argument('--client',type=str,required=True)
+parser.add_argument('--server',type=str,required=True)
+
+args = parser.parse_args()
+
+npf_runner(args.stacks,args.client,args.server)
+
